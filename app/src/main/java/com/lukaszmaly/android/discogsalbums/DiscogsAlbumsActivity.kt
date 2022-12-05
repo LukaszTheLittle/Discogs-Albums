b82c7dee07ad0552937212dce558b9c0f4062bc5
LukaszTheLittle <lukis.maly@gmail.com>
2022-12-05 18:11:05 +0100

    First commit

---
+++ app/src/main/java/com/lukaszmaly/android/discogsalbums/DiscogsAlbumsActivity.kt
@@ -0,0 +1,11 @@
+package com.lukaszmaly.android.discogsalbums
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiscogsAlbumsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_discogs_albums)
+    }
+}
