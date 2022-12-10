c55f19d1362124402f02a9576403dc6da4b9c5fa
LukaszTheLittle <lukis.maly@gmail.com>
2022-12-10 15:09:34 +0100

    Moved `fetchContents()` function from `DiscogsAlbumsFragment` class

--- app/src/main/java/com/lukaszmaly/android/discogsalbums/DiscogsFetch.kt
+++ app/src/main/java/com/lukaszmaly/android/discogsalbums/DiscogsFetch.kt
@@ -1,6 +1,12 @@
 package com.lukaszmaly.android.discogsalbums
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.lukaszmaly.android.discogsalbums.api.DiscogsApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
@@ -15,6 +21,27 @@ class DiscogsFetch {
             .build()
 
         discogsApi = retrofit.create(DiscogsApi::class.java)
+    }
+
+    fun fetchContents(): LiveData<String> {
+        val responseLivedata: MutableLiveData<String> = MutableLiveData()
+        val discogsRequest: Call<String> = discogsApi.fetchContents()
+
+        discogsRequest.enqueue(object: Callback<String> {
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch data", t)
+            }
+
+            override fun onResponse(
+                call: Call<String>,
+                response: Response<String>
+            ) {
+                Log.d(TAG, "Response received")
+                responseLivedata.value = response.body()
+            }
+        })
+        return responseLivedata
     }
 
     companion object {
