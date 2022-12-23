d8e023900936252d86e3c7535ba0960e989adf65
LukaszTheLittle <lukis.maly@gmail.com>
2022-12-23 18:06:14 +0100

    Created a model object class

---
+++ app/src/main/java/com/lukaszmaly/android/discogsalbums/AlbumCoverItem.kt
@@ -0,0 +1,8 @@
+package com.lukaszmaly.android.discogsalbums
+
+data class AlbumCoverItem(
+    var releaseId: String = "",
+    var artistName: String = "",
+    var albumTitle: String = "",
+    var url: String = ""
+)
