fc6f50f51f13249e3e8ed6b232c665415202ea5f
LukaszTheLittle <lukis.maly@gmail.com>
2022-12-28 15:28:15 +0100

    cleaned up comments

--- app/src/main/java/com/lukaszmaly/android/discogsalbums/AlbumCoverItem.kt
+++ app/src/main/java/com/lukaszmaly/android/discogsalbums/AlbumCoverItem.kt
@@ -1,8 +1,8 @@
 package com.lukaszmaly.android.discogsalbums
 
 data class AlbumCoverItem(
-    var id: String = "", // release id
-    var name: String = "", // artist name
-    var title: String = "", // album title
-    var resource_url: String = "" // cover photo url
+    var id: String = "",                // release id
+    var name: String = "",              // artist name
+    var title: String = "",             // album title
+    var resource_url: String = ""       // cover photo url
 )
