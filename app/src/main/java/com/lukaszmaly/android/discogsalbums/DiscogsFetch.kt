75e4ce53171e938a2864928390e99deb6869938b
LukaszTheLittle <lukis.maly@gmail.com>
2022-12-28 16:59:27 +0100

    Updated class for Gson

--- app/src/main/java/com/lukaszmaly/android/discogsalbums/DiscogsFetch.kt
+++ app/src/main/java/com/lukaszmaly/android/discogsalbums/DiscogsFetch.kt
@@ -8,7 +8,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class DiscogsFetch {
 
@@ -17,28 +17,29 @@ class DiscogsFetch {
     init {
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://api.discogs.com/")
-            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         discogsApi = retrofit.create(DiscogsApi::class.java)
     }
 
-    fun fetchContents(): LiveData<String> {
-        val responseLivedata: MutableLiveData<String> = MutableLiveData()
-        val discogsRequest: Call<String> = discogsApi.fetchContents()
+    fun fetchContents(): LiveData<ReleaseData> {
+        val responseLivedata: MutableLiveData<ReleaseData> = MutableLiveData()
+        val discogsRequest: Call<ReleaseData> = discogsApi.fetchContents()
 
-        discogsRequest.enqueue(object: Callback<String> {
+        discogsRequest.enqueue(object: Callback<ReleaseData> {
 
-            override fun onFailure(call: Call<String>, t: Throwable) {
+            override fun onFailure(call: Call<ReleaseData>, t: Throwable) {
                 Log.e(TAG, "Failed to fetch data", t)
             }
 
             override fun onResponse(
-                call: Call<String>,
-                response: Response<String>
+                call: Call<ReleaseData>,
+                response: Response<ReleaseData>
             ) {
                 Log.d(TAG, "Response received")
-                responseLivedata.value = response.body()
+                val discogsResponse: ReleaseData? = response.body()
+                responseLivedata.value = discogsResponse
             }
         })
         return responseLivedata
