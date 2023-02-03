8a68de208c6ea1aa79a5e00f1a09724105e7a766
LukaszTheLittle <lukis.maly@gmail.com>
2023-02-03 19:18:11 +0100

    updated `DiscogsApi` with function that accepts a URL as input

--- app/src/main/java/com/lukaszmaly/android/discogsalbums/api/DiscogsApi.kt
+++ app/src/main/java/com/lukaszmaly/android/discogsalbums/api/DiscogsApi.kt
@@ -1,13 +1,18 @@
 package com.lukaszmaly.android.discogsalbums.api
 
 import com.lukaszmaly.android.discogsalbums.ReleaseData
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface DiscogsApi {
 
     @GET(HTTPS_ADDRESS_SPECIFIC_RELEASE)
     fun fetchContents(): Call<ReleaseData>
+
+    @GET
+    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
 
     companion object {
         private const val HTTPS_ADDRESS_SPECIFIC_RELEASE = "/releases/24929597" +
