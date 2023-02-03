aa9b05dddc31fda732447735fef3ddce8e54f5ed
LukaszTheLittle <lukis.maly@gmail.com>
2023-02-03 19:25:42 +0100

    Added thumbnail downloading

--- app/src/main/java/com/lukaszmaly/android/discogsalbums/DiscogsFetch.kt
+++ app/src/main/java/com/lukaszmaly/android/discogsalbums/DiscogsFetch.kt
@@ -1,9 +1,13 @@
 package com.lukaszmaly.android.discogsalbums
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.lukaszmaly.android.discogsalbums.api.DiscogsApi
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -43,6 +47,14 @@ class DiscogsFetch {
             }
         })
         return responseLivedata
+    }
+
+    @WorkerThread
+    fun fetchThumbnail(url: String): Bitmap? {
+        val response: Response<ResponseBody> = discogsApi.fetchUrlBytes(url).execute()
+        val bitmap = response.body()?.byteStream()?.use(BitmapFactory::decodeStream)
+        Log.i(TAG, "Decoded bitmap=$bitmap from Response=$response")
+        return bitmap
     }
 
     companion object {
