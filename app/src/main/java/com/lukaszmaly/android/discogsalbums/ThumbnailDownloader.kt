1482d928192780befe3a96c265c77ee5f97fb6e7
LukaszTheLittle <lukis.maly@gmail.com>
2023-02-06 18:55:45 +0100

    Background thread to receive and process download requests

---
+++ app/src/main/java/com/lukaszmaly/android/discogsalbums/ThumbnailDownloader.kt
@@ -0,0 +1,22 @@
+package com.lukaszmaly.android.discogsalbums
+
+import android.os.HandlerThread
+import android.util.Log
+
+class ThumbnailDownloader<in T>: HandlerThread(TAG) {
+
+    private var hasQuit = false
+
+    override fun quit(): Boolean {
+        hasQuit = true
+        return super.quit()
+    }
+
+    fun queueThumbnail(target: T, url: String) {
+        Log.i(TAG, "Got a URL: $url")
+    }
+
+    companion object {
+        private const val TAG = "ThumbnailDownloader"
+    }
+}
